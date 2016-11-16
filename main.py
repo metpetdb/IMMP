51ec918b0f1d2a6e343fe4880bebca3d397ae827
Theo Browne <me@theo.li>
2016-11-16 13:34:12 -0500

    Fixed home page

--- main.py
+++ main.py
@@ -23,18 +23,7 @@ class Maps(db.Model):
 # Index routing
 @app.route('/')
 def index():
-	return render_template("profile.html")
-
-# Login page routing
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-	error = None
-	if request.method == 'POST':
-		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-			error = 'Invalid credentials. Please try again.'
-		else:
-			return render_template('profile.html', name='admin')
-	return render_template('login.html', error=error)
+	return render_template("index.html")
 
 @app.route('/new_map', methods=['GET', 'POST'])
 def newmap():
