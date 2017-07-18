bcd5c5702189631f66a6058df0500018441ffcde
IreLynn <irelaa2@rpi.edu>
2017-07-18 16:29:00 -0400

    edited verbiage tag==>mapping

--- static/js/main.js
+++ static/js/main.js
@@ -60,7 +60,7 @@ document.getElementById("cancel").addEventListener("click", function(){
 })
 
 document.getElementById("help").addEventListener("click",function(){
-    alert("To add a tag, click anywhere on the image, then click the data row you wish to tag to that location. A red X will appear which allows you to delete tags you've created. Click 'Save' in the bottom right to save your tags once you've finished.")
+    alert("To add a mapping, click anywhere on the image, then click the data row you wish to map to that location. A red X will appear which allows you to delete mappings you've created. Click 'Save' in the bottom right to save your map once you've finished.")
 });
 
 document.getElementById("deleteTags").addEventListener("click", function(){
